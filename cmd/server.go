5af38601d6ec8eafa6c47ec705898b57c28b0489
hihumikan <a@qqey.net>
2024-05-24 00:43:11 +0900

    chore: Add CSV file upload functionality

--- cmd/server.go
+++ cmd/server.go
@@ -1,10 +1,53 @@
-// Package main contains the main work of the cloud controller api service.
 package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
+func saveCSVHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Error retrieving the file", http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+
+	os.MkdirAll("./csv", os.ModePerm)
+
+	dst, err := os.Create(filepath.Join("./csv", "uploaded.csv"))
+	if err != nil {
+		http.Error(w, "Unable to create file on the server", http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, "Error saving the file", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "File uploaded successfully")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/upload", saveCSVHandler)
+	fmt.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
 }
